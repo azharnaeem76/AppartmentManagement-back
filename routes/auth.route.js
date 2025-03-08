50e4f47733c1292f6bba3fc61aea748b66cdd87e
azharnaeem76 <azharnaeem6679@gmail.com>
2025-03-09 00:37:32 +0500

    updates

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,16 +4,14 @@ const residentController = require('../controllers/residentController');
 const router = express.Router();
 
 // Login route for Superadmin
-router.post('/SuperAdminlogin', SuperAdminlogin);
-router.post("/admin/login", adminLogin);
-
+router.post("/login", login);
 router.post("/resident/login", residentController.login)
 router.post("/resident/register", residentController.register)
 router.post("/resident/forgotPassword", residentController.forgotPassword)
 router.post("/resident/register", residentController.forgotPassword);
 router.post('/change-password', residentController.changePassword);
 router.post('/deleteAccount', residentController.deleteAccount);
-
-router.post("/login", login);
+router.post('/SuperAdminlogin', SuperAdminlogin);
+router.post("/admin/login", adminLogin);
 
 module.exports = router;
